4a1dd0e8392f833fec7aada60c4ee7d6df94f521
benesja4 <benes.jakub1@gmail.com>
2020-11-04 11:04:49 +0100

    6. cvičení: Uložení košíku do CSV. Načítání XML pomocí SAX a DOM.

--- src/uhk/fim/gui/MainFrame.java
+++ src/uhk/fim/gui/MainFrame.java
@@ -1,13 +1,21 @@
 package uhk.fim.gui;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 import uhk.fim.model.ShoppingCart;
 import uhk.fim.model.ShoppingCartItem;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.xml.parsers.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
 public class MainFrame extends JFrame implements ActionListener {
     JPanel panelMain;
@@ -112,13 +120,14 @@ public class MainFrame extends JFrame implements ActionListener {
         fileMenu.add(new AbstractAction("Otevřít") {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("Otevřít");
+                //loadFileXmlSax();
+                loadFileXmlDom();
             }
         });
         fileMenu.add(new AbstractAction("Uložit") {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println("Uložit");
+                saveFileCsv();
             }
         });
         menuBar.add(fileMenu);
@@ -140,10 +149,10 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
     private void addProductToCart() {
-        if(!txtInputName.getText().isBlank()) {
+        if (!txtInputName.getText().isBlank()) {
             try {
                 double price = Double.parseDouble(txtInputPricePerPiece.getText().replace(",", "."));
-                if(price > 0) {
+                if (price > 0) {
                     // Vytvořit novou položku
                     ShoppingCartItem item = new ShoppingCartItem(txtInputName.getText(), price, (int) spInputPieces.getValue());
                     // Přidat položku do košíku
@@ -155,7 +164,7 @@ public class MainFrame extends JFrame implements ActionListener {
                 } else {
                     JOptionPane.showMessageDialog(this, "Cena musí být větší než 0", "Chyba", JOptionPane.ERROR_MESSAGE);
                 }
-            } catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(this, "Zadejte správný formát ceny a počtu kusů!", "Chyba", JOptionPane.ERROR_MESSAGE);
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(this, "Došlo k chybě v programu!", "Chyba", JOptionPane.ERROR_MESSAGE);
@@ -167,5 +176,116 @@ public class MainFrame extends JFrame implements ActionListener {
 
     private void updateFooter() {
         lblTotalPrice.setText("Celková cena: " + String.format("%.2f", shoppingCart.getTotalPrice()));
+    }
+
+    // Metoda, která se postará o uložení košíku do formátu CSV
+    private void saveFileCsv() {
+        // Init chooser - okno, které Vám dá možnost zvolit umístění souboru a jeho název
+        JFileChooser fc = new JFileChooser();
+        // Pokud jsme klikli na  tlačítko uložit
+        if (fc.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+            // Načeteme si cestu k souboru
+            String fileName = fc.getSelectedFile().getAbsolutePath();
+            // Try-with-resources https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
+            // Nemusíme uzavírat spojení - uzavře se samo
+            // Používáme buffer, který potřebuje nějaký primitivnější stream
+            try (BufferedWriter bfw = new BufferedWriter(new FileWriter(fileName, true))) {
+                // Projdeme položky
+                for (ShoppingCartItem item : shoppingCart.getItems()) {
+                    // Pro každou položku vytvoříme záznam v CSV
+                    bfw.write(item.getName() + ";" + item.getPricePerPiece() + ";" + item.getPieces());
+                    // Nový řádek
+                    bfw.newLine();
+                }
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Při ukládání došlo k chybě",
+                        "Chyba",
+                        JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Metoda, která načte xml - SAX
+    // Mimo olivy můžete kouknout např. sem http://tutorials.jenkov.com/java-xml/sax-defaulthandler.html
+    private void loadFileXmlSax() {
+        try {
+            // Char buffer, do kterého budeme zapisovat "hodnoty" elementů
+            CharArrayWriter content = new CharArrayWriter();
+            // Vytvoříme SAX parser
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            // Řekneme, který file chceme parsovat a vytvoříme handler, který obslouží události, které budou vznikat při parsování
+            parser.parse(new File("src/uhk/fim/shoppingCart.xml"), new DefaultHandler() {
+                // Parser narazil na otevřený tag
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    content.reset();
+                }
+                // Parser narazil na uzavřený tag
+                @Override
+                public void endElement(String uri, String localName, String qName) throws SAXException {
+                    System.out.println(qName + ": " + content.toString());
+                }
+                // Parser narazil na nějaký řetězec. Pozor, zavolá se i při nalezení odřádkování.
+                @Override
+                public void characters(char[] ch, int start, int length) throws SAXException {
+                    super.characters(ch, start, length);
+                    content.write(ch, start, length);
+                }
+            });
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Metoda, která načte xml - DOM
+    private void loadFileXmlDom() {
+        try {
+            // Vytvoříme builder
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            // Builder má metodu parse, která se postará o vytvoření objektu docoment, který má v sobě celou strukturu XML
+            // Tím jsme si XML uložili do paměti a můžeme s ním dál pracovat = princim DOM
+            Document document = builder.parse(new File("src/uhk/fim/shoppingCart.xml"));
+            // Z dokumentu načteme prvního potomka = root = kořenová element
+            Node root = document.getFirstChild();
+            // Ukázka načtení typu nodu.
+            short nodeType = root.getNodeType();
+            // *** Zde je jen snaha ukázat, že je nutné strukturu projít nějakou rekurzí viz. ukázky v olivě ***
+            // Má kořenový element potomky?
+            if(root.hasChildNodes()){
+                // Ano má - načteme položky do seznamu
+                NodeList list = root.getChildNodes();
+                // Projdeme seznam
+                for(int i = 0; i < list.getLength(); i++) {
+                    // Načteme konkrétní node ze seznamu
+                    Node nextNode = list.item(i);
+                    // Opět se ptáme, jestli má potomky
+                    if(nextNode.hasChildNodes()) {
+                        // Ano má - načteme položky do seznamu
+                        NodeList list2 = nextNode.getChildNodes();
+                        // Projdeme seznam
+                        for(int j = 0; j <= list2.getLength(); j++){
+                            // Načteme konkrétní node ze seznamu
+                            Node nextNode2 = list2.item(j);
+                            /// !!! Tady už můžete vidět, že je ntuné vytvořit nějakou rekurzi !!!
+                            // Nemůžeme strukturu takto ručně projít.
+                        }
+                    }
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
